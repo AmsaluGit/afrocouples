4eed99b65e7d58a9b0ff28dfc45e9522772c5f81
AmsaluGit <tadesseamsalu@gmail.com>
2022-02-01 14:31:20 +0300

    db added

--- choice.sql
+++ choice.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 03, 2022 at 02:34 PM
+-- Generation Time: Feb 01, 2022 at 10:08 AM
 -- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 

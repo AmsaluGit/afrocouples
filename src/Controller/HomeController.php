8a6934a477477493f2e89b893982cd5ed3e0d86c
AmsaluGit <tadesseamsalu@gmail.com>
2022-03-13 16:44:39 +0300

    manifest file added

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -17,9 +17,59 @@ use App\Form\UserType;
 use App\Form\GalleryType;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+ 
 
 class HomeController extends AbstractController
 {
+    /**
+     * @Route("/send", name="send")
+     */
+    public function send(): Response
+    {
+         //sending.. 
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+        $channel->queue_declare('hello', false, false, false, false);
+
+        $msg = new AMQPMessage('Hello World!');
+        $channel->basic_publish($msg, '', 'hello');
+
+        echo " [x] Sent 'Hello World!'\n";
+        $channel->close();
+        $connection->close();
+        return new Response("sent");
+    }
+    /**
+     * @Route("/receive", name="receive")
+     */
+    public function message(): Response
+    {
+        //receiving..
+        $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+        $channel = $connection->channel();
+        
+        $channel->queue_declare('hello', false, false, false, false);
+        
+        echo " [*] Waiting for messages. To exit press CTRL+C\n";
+        
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+        };
+        
+        $channel->basic_consume('hello', '', false, true, false, false, $callback);
+        
+        while ($channel->is_open()) {
+            $channel->wait();
+        }
+        
+        $channel->close();
+        $connection->close();
+        return new Response("received");
+
+    }
     /**
      * @Route("/", name="home")
      */
